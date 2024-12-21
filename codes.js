083d2ba7726ca833ad3398f57133ce39153b3cb3
DucHung821 <duchung567@gmail.com>
2024-12-21 08:56:26 +0700

    Update codes.js

--- codes.js
+++ codes.js
@@ -1,358 +1,105 @@
 {
-  "incorrect_codes": [],
-  "existing_codes": [],
-  "codes": [
-     {
-        "name": "Hidden Money Secrets to Achieve Financial Freedom",
-        "code": "40189"
-    },
-    {
-        "name": "The 5 Secret Weapons Whales Use to Control the Market | Part 2 of 6",
-        "code": "45103"
-    },
-    {
-        "name": "Altcoins: Which Will Take Over the Crypto World? | Part 3 of 6",
-        "code": "16753"
-    },
-    {
-        "name": "5 Tools to Track Big Moves & Protect Your Gains | Crypto Whales | Part 3 of 6",
-        "code": "73094"
-    },
-    {
-        "name": "The Power of Words: Building Deep Connections Through Communication",
-        "code": "38193"
-    },
-    {
-        "name": "How Altcoins Are Disrupting Traditional Finance in 10 Big Ways | Part 4 of 6",
-        "code": "47309"
-    },
-    {
-        "name": "From Limitations to Growth: How to Transform Your Mindset",
-        "code": "98587"
-    },
-    {
-        "name": "Top 10 Richest People in Crypto – You’ll Be Shocked! | Crypto Whales | Part 4 of 6",
-        "code": "97815"
-    },
-    {
-        "name": "Small Investment, Big Returns: Business Ideas for Women",
-        "code": "71940"
-    },
-    {
-        "name": "The Truth About Altcoins: Massive Rewards, Massive Risks | Part 5 of 6",
-        "code": "44198"
-    },
-    {
-        "name": "What’s a 51% Attack? Protect Your Crypto",
-        "code": "14310"
-    },
-    {
-        "name": "Avoid Whale Traps! Top Strategies to Protect Your Crypto | Part 5 of 6",
-        "code": "89931"
-    },
-    {
-        "name": "Stock Market Secrets: Earning Money Starting with $100",
-        "code": "71930"
-    },
-    {
-        "name": "Airdrop Soon! Where to Find Free Tokens (and What to Avoid!)",
-        "code": "55214"
-    },
-    {
-        "name": "Bitcoin Maximalist or Dex Trader? Find the Wallet Made for You | Part 6 of 6",
-        "code": "21740"
-    },
-    {
-        "name": "How to Reach $100K: Smart Money Moves for Teenagers",
-        "code": "32157"
-    },
-    {
-        "name": "The Hidden Dangers of Whale Tracking: What They Don’t Want You to Know | Part 6 of 6",
-        "code": "10248"
-    },
-    {
-        "name": "How to Make Money Just by Enjoying Music",
-        "code": "12729"
-    },
-    {
-        "name": "No Trading, No Stress: Passive Income with Staking 101 | Part 1 of 6",
-        "code": "40203"
-    },
-    {
-        "name": "How to Keep Your Crypto Rock-Solid in Any Market | Part 1 of 6",
-        "code": "43881"
-    },
-    {
-        "name": "Sleep Hacks for Success: Rest Better, Achieve More",
-        "code": "48103"
-    },
-    {
-        "name": "Staking Secrets Revealed: How You Earn Just by Holding Crypto | Part 2 of 6",
-        "code": "66506"
-    },
-    {
-        "name": "Play and Earn: Genuine Game Apps That Pay You Real Money",
-        "code": "32113"
-    },
-    {
-        "name": "Top 10 Stablecoins: Who’s Ruling the Market Right Now? | Part 2 of 6",
-        "code": "96724"
-    },
-    {
-        "name": "Unlock These Wealth Secrets to Start Building Riches",
-        "code": "21919"
-    },
-    {
-        "name": "Three Faces of Staking: Find Your Perfect Fit | Part 3 of 6",
-        "code": "93662"
-    },
-    {
-        "name": "How USDT Became Crypto’s Top Stablecoin – Secrets Inside | Part 3 of 6",
-        "code": "10023"
-    },
-    {
-        "name": "Politifi’s Hottest Coin: $DUM Skyrocket 100X?",
-        "code": "37650"
-    },
-    {
-        "name": "How to Improve Your Relationships Today",
-        "code": "41820"
-    },
-    {
-        "name": "Digital Gold for Retirees? Florida Goes Crypto",
-        "code": "01239"
-    },
-    {
-        "name": "5 Perks of Staking: How Crypto Pays You for Patience | Part 4 of 6",
-        "code": "12996"
-    },
-    {
-        "name": "Reprogram Your Mind: Unlock a Positive Mindset",
-        "code": "27589"
-    },
-    {
-        "name": "USDC: The Most Transparent Stablecoin in Crypto | Part 4 of 6",
-        "code": "14893"
-    },
-    {
-        "name": "Start Your Dream Business as a Woman for Under $100!",
-        "code": "81729"
-    },
-    {
-        "name": "Is Staking Safe? Real-Life Risks and Lessons You Need | Part 5 of 6",
-        "code": "01034"
-    },
-    {
-        "name": "USDT vs USDC: Making the Right Choice for Your Crypto | Part 5 of 6",
-        "code": "58917"
-    },
-    {
-        "name": "The Ultimate Guide to Earning from Stocks",
-        "code": "77182"
-    },
-    {
-        "name": "Staking for Beginners: Earn Your First $100 Step-by-Step | Part 6 of 6",
-        "code": "81594"
-    },
-    {
-        "name": "Unlocking Wealth: How Teenagers Can Make $100K",
-        "code": "12455"
-    },
-    {
-        "name": "Maximize Your Stablecoins: Top Strategies with USDT & USDC | Part 6 of 6",
-        "code": "46233"
-    },
-    {
-        "name": "Secret Guide to Making Money with Music",
-        "code": "21849"
-    },
-    {
-        "name": "Memecoins Unmasked: From Joke to Jackpot | Part 1 of 6",
-        "code": "51049"
-    },
-    {
-        "name": "Unlock Earning Potential with Avalanche’s Latest Visa Crypto Card!",
-        "code": "81056"
-    },
-    {
-        "name": "Why FOMO and FUD Control the Crypto Market – and How to Beat Them | Part 1 of 10",
-        "code": "19740"
-    },
-    {
-        "name": "From Ideas to Income: How Teens Can Earn $100K",
-        "code": "12681"
-    },
-    {
-        "name": "Investors Burned by Jump Trading’s DIO Token Dump",
-        "code": "11771"
-    },
-    {
-        "name": "Top 5 Memecoins Shaping the Market | Part 2 of 6",
-        "code": "66921"
-    },
-    {
-        "name": "How to Earn $5,000 Per Month by Listening to Music",
-        "code": "21638"
-    },
-    {
-        "name": "Millionaire Mindset: Secret Habits That Lead to Wealth",
-        "code": "22415"
-    },
-    {
-        "name": "Why Memecoins Go Viral – The Psychology Behind the Hype | Part 4 of 6",
-        "code": "44229"
-    },
-    {
-        "name": "Yolo Culture in Crypto: The Apes and Degens Who Risk It All | Part 2 of 10",
-        "code": "75661"
-    },
-    {
-        "name": "Earn $5,000 a Month in Just 10 Minutes a Day with Pinterest",
-        "code": "21471"
-    },
-    {
-        "name": "5 Key Strategies to Catch Memecoin Waves Early | Part 5 of 6",
-        "code": "13895"
-    },
-    {
-        "name": "Future of Finance: Earn and Borrow on Your Terms with DeFi | Part 1 of 6",
-        "code": "10687"
-    },
-    {
-        "name": "Get Paid $30 for Every Word You Type Online",
-        "code": "21473"
-    },
-    {
-        "name": "What Powers DeFi? The 5 Pillars Building Financial Freedom | Part 2 of 6",
-        "code": "98045"
-    },
-    {
-        "name": "Adopt These Habits to Become a Millionaire",
-        "code": "41728"
-    },
-    {
-        "name": "Memecoin Risks: How to Avoid Costly Mistakes | Part 6 of 6",
-        "code": "35219"
-    },
-    {
-        "name": "The Worst Rekt Stories Ever – Lessons Learned the Hard Way | Part 3 of 10",
-        "code": "18204"
-    },
-    {
-        "name": "The Mystery and Rumors of Russell Token",
-        "code": "72015"
-    },
-	{
-        "name": "HOW TO MAKE $5000 MONTHLY SPENDING ONLY 10 MINUTES A DAY ON PINTEREST | MEMEGIRLS",
-        "code": "82193"
-    },
-    {
-        "name": "DEFI VS BANKS: WHY TRADITIONAL FINANCE CAN’T COMPETE | PART 3 OF 6 | MEMEFI",
-        "code": "71937"
-    },
-    {
-        "name": "HOW TO MAKE $10,000 PER MONTH WITH TYPING (FULL GUIDE) | MEMEGIRLS",
-        "code": "12371"
-    },
-    {
-        "name": "WHY 99% OF CRYPTO HOLDERS ARE AT RISK – SECURE YOUR FUNDS TODAY | PART 1 OF 6 | MEMEFI",
-        "code": "70917"
-    },
-    {
-        "name": "MEMECOIN OR SHITCOIN? THE DIFFERENCE COULD SAVE YOU | PART 3 OF 6 | MEMEFI",
-        "code": "42785"
-    },
-    {
-        "name": "WANT TO BE A MILLIONAIRE? START THESE RIGHT NOW | MEMEGIRLS",
-        "code": "71823"
-    },
-    {
-        "name": "WEN MOON, WEN LAMBO? HOW MOON BOYS KEEP THE DREAM ALIVE | PART 4 OF 10 | MEMEFI",
-        "code": "86132"
-    },
-    {
-        "name": "ONE PHISHING LINK COST HIM $50,000 – LEARN FROM HIS MISTAKE | PART 2 OF 6 | MEMEFI",
-        "code": "03854"
-    },
-    {
-        "name": "DEFI’S SECRET WEAPONS: SMART CONTRACTS AND DAPPS | PART 4 OF 6 | MEMEFI",
-        "code": "61834"
-    },
-    {
-        "name": "LOW-COST BUSINESS IDEAS TO START WITH LESS THAN $100 | MEMEGIRLS",
-        "code": "61733"
-    },
-    {
-        "name": "$220 MILLION HARD DRIVE DISASTER: DON’T LET THIS BE YOU | PART 3 OF 6 | MEMEFI",
-        "code": "10954"
-    },
-    {
-        "name": "TOP DROPSHIPPING NICHES YOU SHOULD START RIGHT NOW | MEMEGIRLS",
-        "code": "71649"
-    },
-    {
-        "name": "DIAMOND HANDS & PAPER HANDS: WHO REALLY WINS IN CRYPTO? | PART 5 OF 10 | MEMEFI",
-        "code": "47591"
-    },
-    {
-        "name": "TOKENS AND STABLECOINS: THE BACKBONE OF DEFI EXPLAINED | PART 5 OF 6 | MEMEFI",
-        "code": "84205"
-    },
-    {
-        "name": "GET PAID TO WALK WITH THESE MONEY-MAKING APPS | MEMEGIRLS",
-        "code": "71391"
-    },
-    {
-        "name": "CRYPTO’S DARK SIDE: PUMPS, DUMPS, AND RUG PULLS | PART 6 OF 10 | MEMEFI",
-        "code": "78409"
-    },
-    {
-        "name": "ULTIMATE APPS FOR MANAGING YOUR FINANCES AND SAVING MONEY | MEMEGIRLS",
-        "code": "81933"
-    },
-    {
-        "name": "HOW TO USE REAL DEFI PLATFORMS FOR MAXIMUM BENEFIT | PART 6 OF 6 | MEMEFI",
-        "code": "41046"
-    },
-    {
-        "name": "AI PUMPS MEMECOIN GOAT TO $700M—FUTURE OF CRYPTO? | MEMIFI",
-        "code": "38507"
-    },
-    {
-        "name": "COPIUM AND HOPIUM: THE TWO EMOTIONAL DRUGS OF CRYPTO | PART 7 OF 10 | MEMEFI",
-        "code": "25033"
-    },
-    {
-        "name": "STEER CLEAR OF THIS IF YOU WANT TO GET RICH | MEMEGIRLS",
-        "code": "63718"
-    },
-    {
-        "name": "TOP 5 CRYPTO WALLETS EXPERTS TRUST WITH MILLIONS | PART 4 OF 6 | MEMEFI",
-        "code": "96325"
-    },
-    {
-        "name": "HOW TO MAKE $50 FOR EVERY SURVEY YOU DO ONLINE | MEMEGIRLS",
-        "code": "67841"
-    },
-    {
-        "name": "EARN THOUSANDS SELLING SIMPLE PICS? THE NFT REVOLUTION BEGINS! | PART 1 OF 5 | MEMEFI",
-        "code": "35058"
-    },
-    {
-        "name": "GET PAID TO REVIEW PRODUCTS ONLINE (FULL GUIDE) | MEMEGIRLS",
-        "code": "63714"
-    },
-    {
-        "name": "5 MUST-KNOW TIPS TO KEEP YOUR CRYPTO SAFE FOREVER | PART 5 OF 6 | MEMEFI",
-        "code": "58223"
-    },
-    {
-        "name": "TOP APPS TO GET CASH BACK ON EVERY PURCHASE | MEMEGIRLS",
-        "code": "23418"
-    },
-    {
-        "name": "OPENSEA’S SECRETS: WHY NFT MARKETPLACE RULES | PART 2 OF 5 | MEMEFI",
-        "code": "73129"
-    }
-  ]
+    "How to Analyze Crypto": "VALUE",
+    "What’s DAO?": "N/A",
+    "Ton voices live": "I LOVE BLUM",
+    "Forks Explained": "GO GET",
+    "Secure your Crypto!": "BEST PROJECT EVER",
+    "Navigating Crypto": "HEYBLUM",
+    "What are Telegram Mini Apps?": "CRYPTOBLUM",
+    "Say No to Rug Pull!": "SUPERBLUM",
+    "Liquidity Pools Guide": "BLUMERSSS",
+    "$2.5M+ DOGS Airdrop": "HAPPYDOGS",
+    "Doxxing? What's that?": "NODOXXING",
+    "Pre-Market Trading?": "WOWBLUM",
+    "Play Track and Type Track Name": "BLUM – BIG CITY LIFE",
+    "How to Memecoin?": "MEMEBLUM",
+    "Token Burning: How & Why?": "ONFIRE",
+    "Bitcoin Rainbow Chart": "SOBLUM",
+    "Crypto Terms. Part 1": "BLUMEXPLORER",
+    "How to Trade Perps?": "CRYPTOFAN",
+    "Sharding Explained": "BLUMTASTIC",
+    "DeFi Explained": "BLUMFORCE",
+    "How To Find Altcoins?": "ULTRABLUM",
+    "Crypto Slang. Part 1": "BLUMSTORM",
+    "What is On-chain Analysis?": "BLUMEXTRA",
+    "Pumptober Special": "PUMPIT",
+    "DeFi Risks: Key Insights": "BLUMHELPS",
+    "Crypto Slang. Part 2": "FOMOOO",
+    "What’s Crypto DEX?": "DEXXX",
+    "Choosing a Crypto Exchange": "CRYPTOZONE",
+    "Node Sales in Crypto": "BLUMIFY",
+    "Understanding Gas Fees": "CRYPTOGAS",
+    "What is Slippage?": "CRYPTOBUZZ",
+    "What’s Next for DeFi?": "BLUMNOW",
+    "Smart Contracts 101": "SMARTBLUM",
+    "Crypto Slang. Part 3": "BOOBLUM",
+    "Regulation: Yay or Nay?": "BLUMSSS",
+    "DEX History": "GODEX",
+    "Crypto Regulations #2": "BLUMRULES",
+    "P2P Trading Safety Tips": "BLUMTIPS",
+    "Crypto Communities": "BLUMMUNITY",
+    "Dec 6 Crypto News": "HUNDRED",
+    "Dex Evolution": "BLUMSPARK",
+    "Crypto Slang. Part 4": "LAMBOBLUM",
+    "What Are AMMs?": "CRYPTOSMART",
+    "Memepad Tutorial": "MEMEPAD",
+    "DEX History #3": "LOVEBLUM",
+    "Blum CMO @ Blockchain Life": "BLUMISLIFE",
+    "Is Binance a DEX?": "BLUMIES",
+    "Crypto Slang. Part 5": "GONNABLUM",
+    "Crypto in Everyday Life": "BLUMANCE",
+    "Trade any memecoin": "N/A",
+    "Launch a memecoin": "N/A",
+  "How to analyze crypto?": "VALUE",
+  "Forks Explained": "GO GET",
+  "Secure your Crypto": "BEST PROJECT EVER",
+  "Navigating crypto": "HEYBLUM",
+  "What are Telegram Mini Apps?": "CRYPTOBLUM",
+  "Say No to Rug Pull!": "superblum",
+  "What Are AMMs?": "CRYPTOSMART",
+  "Liquidity Pools Guide": "BLUMERSSS",
+  "$2.5M+ DOGS Airdrop": "HAPPYDOGS",
+  "Doxxing? What's that?": "NODOXXING",
+  "Pre-Market Trading?": "WOWBLUM",
+  "Play Track & type track...": "blum - big city life",
+  "How to Memecoin?": "MEMEBLUM",
+  "Token Burning: How & Why?": "ONFIRE",
+  "Bitcoin Rainbow Chart?": "SOBLUM",
+  "Crypto Terms. Part 1": "BLUMEXPLORER",
+  "How to trade Perps?": "CRYPTOFAN",
+  "Sharding Explained": "BLUMTASTIC",
+  "DeFi Explained": "BLUMFORCE",
+  "How To Find Altcoins?": "ULTRABLUM",
+  "What's On-chain analysis?": "Blumextra",
+  "Pumptober Special": "Pumpit",
+  "DeFi Risks: Key insights": "Blumhelps",
+  "Crypto Slang. Part 2": "FOMOOO",
+  "Choosing a Crypto Exchange": "CRYPTOZONE",
+  "Node Sales in Crypto?": "Blumify",
+  "What Crypto Dex?": "DEXXX",
+  "Understanding Gas fees": "Cryptogas",
+  "What is Slippage?": "Cryptobuzz",
+  "What’s Next for DeFi?": "BLUMNOW",
+  "Smart Contracts 101": "Smartblum",
+  "Crypto Slang. Part 3": "BOOBLUM",
+  "Regulation: Yay or...": "Blumsss",
+  "DEX History": "Godex",
+  "Crypto Regulation #2": "Blumrules",
+  "P2P Trading Safety Tips": "BLUMTIPS",
+  "Crypto Communities": "Blummunity",
+  "is Binance a Dex?": "Blumies",
+  "Dec 6 Crypto News": "Hundred",
+  "Task: Dex Evolution": "Blumspark",
+  "Crypto Slang. Part 4": "LAMBOBLUM",
+  "Memepad Tutorial": "Memepad",
+  "DEX History #3": "LOVEBLUM",
+  "Dec 12 News": "RIPPLE",
+  "Dec 13 News": "BITCOINJESUS",
+  "Blum CMO @ Blockchain Life": "BLUMISLIFE",
+  "Dec 16 News": "BITCOIN",
+  "Crypto in Everyday Life": "Blumance",
+  "Dec 17 News": "KENDRICK",
+  "Dec 18 News": "Mark",
+  "Crypto Slang. Part 5": "GONNABLUM",
+  "What is Uniswap?": "BLUMSHINE
 }
